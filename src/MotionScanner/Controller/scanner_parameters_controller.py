89fef4ce7dba874c1d4185372639b9701c96f7fa
greek <corpusrincon@gmail.com>
2020-08-28 21:22:43 -0500

    Finishing design controls of parameters and joints.

--- src/MotionScanner/Controller/scanner_parameters_controller.py
+++ src/MotionScanner/Controller/scanner_parameters_controller.py
@@ -1,6 +1,6 @@
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
-from body_button_widget import BodyButtonWidget
+from body_button import BodyButton
 
 
 class ScannerParametersController(QWidget):
@@ -8,6 +8,7 @@ class ScannerParametersController(QWidget):
     def __init__(self, parent=None):         
         super(ScannerParametersController, self).__init__(parent)  
         self.SetupUI()
+        self.setStyleSheet(open('../Lib/stylesheet.css').read())
 
 
     def SetupUI(self):
@@ -15,24 +16,33 @@ class ScannerParametersController(QWidget):
         self._title_layout = QVBoxLayout()
         self._controls_layout = QHBoxLayout()
 
-        self._controls_group = QGroupBox('Measurement parameters')
+        self._controls_group = QGroupBox()
+        self.resizeWidget(self._controls_group,350,600)
+        self._controls_group.setStyleSheet("""background: #757575;""")
         self._main_controls_layout = QVBoxLayout()
 
-        self._length_checkbox = QCheckBox('Length')
-        self._angle_checkbox = QCheckBox('Angles')
-        self._speed_checkbox = QCheckBox('Speed')
-        self._path_checkbox = QCheckBox('Motion path')
-        self._metadata_checkbox = QCheckBox('Metadata')
-        self._performance_evaluation_checkbox = QCheckBox('Performance evaluation')
-        self._audio_steps_checkbox = QCheckBox('Audio steps')
+        width = 300
+        height = 50
 
-        self._main_controls_layout.addWidget(self._length_checkbox)
-        self._main_controls_layout.addWidget(self._angle_checkbox)
-        self._main_controls_layout.addWidget(self._speed_checkbox)
-        self._main_controls_layout.addWidget(self._path_checkbox)
-        self._main_controls_layout.addWidget(self._metadata_checkbox)
-        self._main_controls_layout.addWidget(self._performance_evaluation_checkbox)
-        self._main_controls_layout.addWidget(self._audio_steps_checkbox)
+        self._measurement_parameters = BodyButton(label='Measurement', width=width, height=height, isParameter=True)
+        self._length_parameter = BodyButton(label='Length', width=width, height=height, isParameter=True)
+        self._angle_parameter = BodyButton(label='Angles', width=width, height=height, isParameter=True)
+        self._speed_parameter = BodyButton(label='Speed', width=width, height=height, isParameter=True)
+        self._path_parameter = BodyButton(label='MotionPath', width=width, height=height, isParameter=True)
+        self._metadata_parameter = BodyButton(label='Metadata', width=width, height=height, isParameter=True)
+        self._performance_evaluation_parameter = BodyButton(label='Performance', width=width, height=height, isParameter=True)
+        self._audio_steps_parameter = BodyButton(label='AudioSteps', width=width, height=height, isParameter=True)
+        self._aux_button = BodyButton(label='', width=width, height=20, isParameter=True)
+
+        self._main_controls_layout.addWidget(self._measurement_parameters)
+        self._main_controls_layout.addWidget(self._aux_button)
+        self._main_controls_layout.addWidget(self._length_parameter)
+        self._main_controls_layout.addWidget(self._angle_parameter)
+        self._main_controls_layout.addWidget(self._speed_parameter)
+        self._main_controls_layout.addWidget(self._path_parameter)
+        self._main_controls_layout.addWidget(self._metadata_parameter)
+        self._main_controls_layout.addWidget(self._performance_evaluation_parameter)
+        self._main_controls_layout.addWidget(self._audio_steps_parameter)
         self._main_controls_layout.addStretch()
 
         self._controls_group.setLayout(self._main_controls_layout)
@@ -40,22 +50,22 @@ class ScannerParametersController(QWidget):
         self._avatar_layout = QVBoxLayout()
         self._avatar_group = QGroupBox()
         self.resizeWidget(self._avatar_group,300,600)
-        self._avatar_group.setStyleSheet("""background-image: url(../Lib/human_figure_36.jpg)""")
+        self._avatar_group.setStyleSheet("""background-image: url(../Lib/Images/human_figure_36.jpg)""")
 
         self._avatar_vertical_layout = QVBoxLayout()
 
         # HEAD
         self._head_layout = QHBoxLayout()
-        self._head_button = BodyButtonWidget(width=45,height=45)
+        self._head_button = BodyButton(width=45,height=45)
         self._head_layout.addStretch()
         self._head_layout.addWidget(self._head_button)
         self._head_layout.addStretch()
 
         # SHOULDERS
         self._sholuder_layout = QHBoxLayout()
-        self._left_shoulder_button = BodyButtonWidget(width=45,height=45)
-        self._chest_button = BodyButtonWidget(width=45,height=45)
-        self._right_shooulder_button = BodyButtonWidget(width=45,height=45)
+        self._left_shoulder_button = BodyButton(width=45,height=45)
+        self._chest_button = BodyButton(width=45,height=45)
+        self._right_shooulder_button = BodyButton(width=45,height=45)
         self._sholuder_layout.addStretch()
         self._sholuder_layout.addWidget(self._left_shoulder_button)
         self._sholuder_layout.addWidget(self._chest_button)
@@ -64,8 +74,8 @@ class ScannerParametersController(QWidget):
 
         # ELBOWS
         self._elbow_layout = QHBoxLayout()
-        self._left_elbow_button = BodyButtonWidget()
-        self._right_elbow_button = BodyButtonWidget()
+        self._left_elbow_button = BodyButton(width=50,height=50)
+        self._right_elbow_button = BodyButton(width=50,height=50)
         self._elbow_layout.addStretch()
         self._elbow_layout.addWidget(self._left_elbow_button)
         self._elbow_layout.addStretch()
@@ -74,23 +84,28 @@ class ScannerParametersController(QWidget):
 
         # WRIST
         self._wrist_layout = QHBoxLayout()
-        self._left_wrist_button = BodyButtonWidget()
-        self._left_hip_button = BodyButtonWidget()
-        self._center_button = BodyButtonWidget()
-        self._right_hip_button = BodyButtonWidget()
-        self._right_wrist_button = BodyButtonWidget()
+        self._left_wrist_button = BodyButton()
+        self._left_hip_button = BodyButton()
+        self._center_button = BodyButton()
+        self._right_hip_button = BodyButton()
+        self._right_wrist_button = BodyButton()
+        self._wrist_layout.addStretch()
         self._wrist_layout.addStretch()
         self._wrist_layout.addWidget(self._left_wrist_button)
+        self._wrist_layout.addStretch()
         self._wrist_layout.addWidget(self._left_hip_button)
-        self._wrist_layout.addWidget(self._center_button)
+        self._wrist_layout.addStretch()
+        # self._wrist_layout.addWidget(self._center_button)
         self._wrist_layout.addWidget(self._right_hip_button)
+        self._wrist_layout.addStretch()
         self._wrist_layout.addWidget(self._right_wrist_button)
+        self._wrist_layout.addStretch()
         self._wrist_layout.addStretch()
 
         # KNEES
         self._knee_layout = QHBoxLayout()
-        self._left_knee_button = BodyButtonWidget()
-        self._right_knee_button = BodyButtonWidget()
+        self._left_knee_button = BodyButton()
+        self._right_knee_button = BodyButton()
         self._knee_layout.addStretch()
         self._knee_layout.addWidget(self._left_knee_button)
         self._knee_layout.addWidget(self._right_knee_button)
@@ -98,8 +113,8 @@ class ScannerParametersController(QWidget):
 
         # ANKLES
         self._ankle_layout = QHBoxLayout()
-        self._left_ankle_button = BodyButtonWidget()
-        self._right_ankle_button = BodyButtonWidget()
+        self._left_ankle_button = BodyButton()
+        self._right_ankle_button = BodyButton()
         self._ankle_layout.addStretch()
         self._ankle_layout.addWidget(self._left_ankle_button)
         self._ankle_layout.addWidget(self._right_ankle_button)
@@ -109,7 +124,9 @@ class ScannerParametersController(QWidget):
         self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addLayout(self._head_layout)
         self._avatar_vertical_layout.addStretch()
+        self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addLayout(self._sholuder_layout)
+        self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addLayout(self._elbow_layout)
         self._avatar_vertical_layout.addStretch()
@@ -118,11 +135,17 @@ class ScannerParametersController(QWidget):
         self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addStretch()
+        self._avatar_vertical_layout.addStretch()
+        self._avatar_vertical_layout.addStretch()
+        self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addLayout(self._knee_layout)
         self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addStretch()
+        self._avatar_vertical_layout.addStretch()
+        self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addLayout(self._ankle_layout)
+        self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addStretch()
         self._avatar_vertical_layout.addStretch()
 
---
+++ src/MotionScanner/Lib/Catalogs/catalog_parameter_label.py
@@ -0,0 +1,10 @@
+
+
+class CatalogParameterLabel(object):
+
+    @staticmethod
+    def test():
+        print('OSO test')
+
+    # LENGTH_ACTIVE = """background:transparent;background-image:url('../Lib/length_active.png');border: none;"""
+    # LENGTH_NOT_ACTIVE = """background:transparent;background-image:url('../Lib/length_active.png');border: none;"""
